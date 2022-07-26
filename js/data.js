2408c9c608edafd60a6770a8fd4ac0f663baaf58
Jonathan Perez Hernandez <jonathan_perez_hdz@hotmail.com>
2022-07-26 02:25:43 -0500

    Change portfolio, objectives and technologies

--- js/data.js
+++ js/data.js
@@ -1,96 +1,91 @@
 export const projectList = [
-  {
-    title: "Portfolio",
-    description: "My web portfolio.",
-    image: 'assets/images/project-portfolio.png',
-    view: "https://jony-ph.github.io/Portfolio",
-    code: "https://github.com/jony-ph/Portfolio",
-    techs: [
-      "assets/images/icon-html.svg",
-      "assets/images/icon-css.svg",
-      "assets/images/icon-javascript.svg"
-    ]
-  },
-  {
-    title: "Discord Bot",
-    description: "A Discord Bot to play music, searches, moderation and more.",
-    image: 'assets/images/project-discord.png',
-    view: "https://discord.com/api/oauth2/authorize?client_id=796251310973255740&permissions=8&scope=bot",
-    code: "https://github.com/jony-ph/Neo-Bot",
-    techs: [
-      "assets/images/icon-python.svg",
-    ]
-  },
-  {
-    title: "Web Store",
-    description: "A web store to manage your products.",
-    image: 'assets/images/project-store.png',
-    view: "#",
-    code: "https://github.com/jony-ph/almacen-web",
-    techs: [
-      "assets/images/icon-html.svg",
-      "assets/images/icon-css.svg",
-      "assets/images/icon-bootstrap.svg",
-      "assets/images/icon-javascript.svg",
-      "assets/images/icon-php.svg",
-      "assets/images/icon-mariadb.svg"
-    ]
-  },
-  {
-    title: "File Server",
-    description: "Web file server.",
-    image: 'assets/images/project-fileserver.png',
-    view: "#",
-    code: "#",
-    techs: [
-      "assets/images/icon-html.svg",
-      "assets/images/icon-css.svg",
-      "assets/images/icon-bootstrap.svg",
-      "assets/images/icon-react.svg",
-      "assets/images/icon-nodejs.svg",
-    ]
-  }
-]
+	{
+		title: "File Server",
+		description: "Web file server.",
+		image: "assets/images/project-fileserver.png",
+		view: "https://transcendent-meerkat-bada2d.netlify.app/",
+		code: "https://github.com/jony-ph/file-server-frontend",
+		techs: [
+			"assets/images/icon-html.svg",
+			"assets/images/icon-css.svg",
+			"assets/images/icon-bootstrap.svg",
+			"assets/images/icon-react.svg",
+			"assets/images/icon-nodejs.svg",
+		],
+	},
+	{
+		title: "Clinical Map",
+		description:
+			"API to obtain information about studies that have been carried out around the world on COVID-19.",
+		image: "assets/images/project-clinical-map.jpg",
+		view: "https://clinicalmapapi.azurewebsites.net",
+		code: "https://github.com/jony-ph/clinical-map-api",
+		techs: ["assets/images/icon-nodejs.svg", "assets/images/icon-mongodb.svg"],
+	},
+	{
+		title: "Web Store",
+		description: "A web store to manage your products.",
+		image: "assets/images/project-store.png",
+		view: "https://junglalmacen.com",
+		code: "https://github.com/jony-ph/almacen-web",
+		techs: [
+			"assets/images/icon-html.svg",
+			"assets/images/icon-css.svg",
+			"assets/images/icon-bootstrap.svg",
+			"assets/images/icon-javascript.svg",
+			"assets/images/icon-php.svg",
+			"assets/images/icon-mariadb.svg",
+		],
+	},
+	{
+		title: "Discord Bot",
+		description: "A Discord Bot to play music, searches, moderation and more.",
+		image: "assets/images/project-discord.png",
+		view: "https://discord.com/api/oauth2/authorize?client_id=796251310973255740&permissions=8&scope=bot",
+		code: "https://github.com/jony-ph/Neo-Bot",
+		techs: ["assets/images/icon-python.svg"],
+	},
+];
 
 export const techsList = [
-  {
-    name: "HTML",
-    icon: "fa-html5"
-  },
-  {
-    name: "CSS",
-    icon: "fa-css3"
-  },
-  {
-    name: "JavaScript",
-    icon: "fa-js"
-  },
-  {
-    name: "React",
-    icon: "fa-react"
-  },
-  {
-    name: "Node JS",
-    icon: "fa-node-js"
-  },
-  {
-    name: "Python",
-    icon: "fa-python"
-  },
-  {
-    name: "Java",
-    icon: "fa-java"
-  },
-  {
-    name: "C/C++",
-    icon: "icon-cplusplus"
-  },
-  {
-    name: "PHP",
-    icon: "fa-php"
-  },
-  {
-    name: "MySQL",
-    icon: "icon-mysql"
-  }
-]
+	{
+		name: "HTML",
+		icon: "fa-html5",
+	},
+	{
+		name: "CSS",
+		icon: "fa-css3",
+	},
+	{
+		name: "JavaScript",
+		icon: "fa-js",
+	},
+	{
+		name: "React",
+		icon: "fa-react",
+	},
+	{
+		name: "Node JS",
+		icon: "fa-node-js",
+	},
+	{
+		name: "Python",
+		icon: "fa-python",
+	},
+	{
+		name: "Java",
+		icon: "fa-java",
+	},
+	{
+		name: "C/C++",
+		icon: "icon-cplusplus",
+	},
+	{
+		name: "PHP",
+		icon: "fa-php",
+	},
+	{
+		name: "MySQL",
+		icon: "icon-mysql",
+	},
+];
